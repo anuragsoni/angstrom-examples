59e2b0cb440dd2de126bd995122044d77d4bfc2f
Anurag Soni <anurag@sonianurag.com>
2019-04-27 22:20:36 -0400

    use applicative interface

--- src/colors.ml
+++ src/colors.ml
@@ -1,24 +1,30 @@
 type t = {red: int; green: int; blue: int}
 
-let hex str =
+let create_color red green blue = {red; green; blue}
+
+(* We use bind here since the result of `take` is required to decide
+   the result of the next parser. *)
+let hex =
   let open Angstrom in
-  match int_of_string_opt ("0x" ^ str) with
-  | Some x -> return x
-  | _ -> fail "hex"
+  take 2
+  >>= fun s ->
+  match int_of_string_opt ("0x" ^ s) with
+  | Some x ->
+      return x
+  | _ ->
+      fail "invalid number"
 
 let color =
   let open Angstrom in
-  (* consume two characters and feed it to the parser returned by the `hex` function. *)
-  let parse_hex = take 2 >>= hex in
-  char '#' (* consume a `#` character and discard it. *)
-  *> lift3
-       (fun red green blue -> {red; green; blue})
-       parse_hex parse_hex parse_hex
-  <?> "color"
+  (* We combine three hex parsers via `apply` since
+     all three parsers are independant. *)
+  create_color <$> char '#' *> hex <*> hex <*> hex <?> "color"
 
 let parse str =
   (* In a real program we would probably do somewith in the error
      scenario instead of raising an exception. *)
   match Angstrom.parse_string color str with
-  | Ok c -> c
-  | Error msg -> failwith msg
+  | Ok c ->
+      c
+  | Error msg ->
+      failwith msg
